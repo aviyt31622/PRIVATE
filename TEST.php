4eba98f5ccd33bc9f12549ef8c5fc3a1801aa42c
AVI YT <109237211+aviyt31622@users.noreply.github.com>
2024-06-13 08:52:00 +0530

    Create TEST.php

---
+++ TEST.php
@@ -0,0 +1,17 @@
+{
+"version" : 16,
+"Sd":"8333612",
+"Less" : true ,
+"Zero" : false ,
+"Shake" : false ,
+"MB" : true,
+"Aim" : false,
+"HS" : true,
+"Ipad" : true,
+"InstantHit" : true,
+"Cross" : true,
+"Xhit" : true,
+"Knock" : false,
+"SitScope" : true,
+"By" : true
+}
